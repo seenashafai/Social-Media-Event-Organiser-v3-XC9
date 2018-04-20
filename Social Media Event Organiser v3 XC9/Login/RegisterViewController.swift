b870119b3357f6e30a9e477f3a40f23f9996556b
seenashafai <seenas@btinternet.com>
2018-04-20 10:26:17 +0100

    Update Registration view

--- Social Media Event Organiser v3 XC9/Login/RegisterViewController.swift
+++ Social Media Event Organiser v3 XC9/Login/RegisterViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func registerAction(_ sender: Any)
+    {
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil{
+                print(error as Any)
+                self.failureAlert()
+                print("firebase validation error")
+            }
+            else
+            {
+                print("success")
+                self.successAlert()
+                
+            }
+        }
+    }
+    
+    func successAlert() {
+        
+        let alertController = UIAlertController(title: "Alert", message: "Registration Successful", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Continue to Sign In", style: .default) { (action:UIAlertAction) in
+            self.navigationController?.popToRootViewController(animated: true);
+        }
+        
+        alertController.addAction(action1)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func failureAlert() {
+        
+        let alertController = UIAlertController(title: "Alert", message: "Registration Failed\nPlease check your email and password", preferredStyle: .alert)
+        
+        let action1 = UIAlertAction(title: "Cancel", style: .default) { (action:UIAlertAction) in
+            print("failed");
+        }
+        
+        alertController.addAction(action1)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +71,6 @@ class RegisterViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
