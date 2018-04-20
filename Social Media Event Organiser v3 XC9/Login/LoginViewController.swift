c67ef788b5c92ec6a462b3c6aaacf312b4e16612
seenashafai <seenas@btinternet.com>
2018-04-20 10:09:24 +0100

    Update pods, Add working authentication

--- Social Media Event Organiser v3 XC9/Login/LoginViewController.swift
+++ Social Media Event Organiser v3 XC9/Login/LoginViewController.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func SignInAction(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil
+            {
+                print(error!)
+            }
+            else{
+                print("login successful")
+                self.performSegue(withIdentifier: "toMainStoryboard", sender: self)
+            }
+        }
+    }
+    
+    @IBAction func RegisterAction(_ sender: Any)
+    {
+        print("To Register View")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +47,6 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
