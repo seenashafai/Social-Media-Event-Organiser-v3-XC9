592160f0e24eb68546146e8a79de3dc5079f6fd8
seenashafai <seenas@btinternet.com>
2018-04-20 08:06:31 +0100

    Initial Commit

---
+++ Social Media Event Organiser v3 XC9Tests/Social_Media_Event_Organiser_v3_XC9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Social_Media_Event_Organiser_v3_XC9Tests.swift
+//  Social Media Event Organiser v3 XC9Tests
+//
+//  Created by Seena Shafai on 20/04/2018.
+//  Copyright © 2018 Seena Shafai. All rights reserved.
+//
+
+import XCTest
+@testable import Social_Media_Event_Organiser_v3_XC9
+
+class Social_Media_Event_Organiser_v3_XC9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
